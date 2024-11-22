ba73f33a0ba1f0e5836fad0c21d9d0b2f7d8463d
DeYan009 <124430267+DeYan009@users.noreply.github.com>
2024-11-22 21:45:12 +0200

    add ex 4

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -4,76 +4,55 @@
 
 int main() 
 {
-	// int n;
+    // // ex 1
+
+    // int n;
 	// std::cin >> n;
     // int mas[10000];
+	// bool prime = false;
+	// bool is_prime = true;
 	// for(int i=0;i<n;i++)
 	// {
 	// 	std::cin >> mas[i];
-	// }
-
-	// for(int i=0;i<n-1;i++)
-	// 	for(int j=i+1;j<n;j++)
-	// 	{
-    //     if (mas[i] < mas[j])
+	// 	is_prime = true;
+	// 	if (!prime)
 	// 		{
-	// 			int tmp = mas[i];
-	// 			mas[i] = mas[j];
-	// 			mas[j] = tmp;
+	// 			if (mas[i] == 1)
+	// 				continue;
+	// 			for(int j=2;j*j<=mas[i];j++)
+	// 			{
+	// 				if (mas[i] % j == 0)
+	// 					{
+	// 						is_prime = false;
+	// 						break;
+	// 					}
+	// 			if (is_prime)
+	// 			{
+	// 				prime = true;
+	// 			}
+	// 			}
+	// 		}
+
+	// }
+	// if (!prime)
+	// 	{
+	// 		for(int i=0;i<n-1;i++)
+	// 			for(int j=i+1;j<n;j++)
+	// 		{
+    //     		if (mas[i] < mas[j])
+	// 			{
+	// 				int tmp = mas[i];
+	// 				mas[i] = mas[j];
+	// 				mas[j] = tmp;
+	// 			}
 	// 		}
 	// 	}
 
 	// for(int i=0;i<n;i++)
 	// 	std::cout << mas[i] << std::endl;
 
-
-    // ex 1
-    int n;
-	std::cin >> n;
-    int mas[10000];
-	bool prime = false;
-	bool is_prime = true;
-	for(int i=0;i<n;i++)
-	{
-		std::cin >> mas[i];
-		is_prime = true;
-		if (!prime)
-			{
-				if (mas[i] == 1)
-					continue;
-				for(int j=2;j*j<=mas[i];j++)
-				{
-					if (mas[i] % j == 0)
-						{
-							is_prime = false;
-							break;
-						}
-				if (is_prime)
-				{
-					prime = true;
-				}
-				}
-			}
-
-	}
-	if (!prime)
-		{
-			for(int i=0;i<n-1;i++)
-				for(int j=i+1;j<n;j++)
-			{
-        		if (mas[i] < mas[j])
-				{
-					int tmp = mas[i];
-					mas[i] = mas[j];
-					mas[j] = tmp;
-				}
-			}
-		}
-
-	for(int i=0;i<n;i++)
-		std::cout << mas[i] << std::endl;
-
     // // ex 2
+
 	// int n;
 	// std::cin >> n;
     // int a[1000];
@@ -151,6 +130,7 @@ int main()
 	// 	std::cout << a[i] << std::endl;
 
     // // ex 3
+
 	// int a[100][100];
 	// int n,m;
 	// int s;
@@ -197,5 +177,75 @@ int main()
 	// // }
     // ex 4
 
+	int n;
+	std::cin >> n;
+    int a[10000];
+	for(int i=0;i<n;i++)
+	{
+		std::cin >> a[i];
+	}
+
+	int j = 0;
+	for(int i=0;i<n;i++)
+	{
+		int num = a[i];
+		int rev_num = 0;
+		int d;
+		while (num > 0)
+		{
+        	d = num % 10;
+        	rev_num = rev_num * 10 + d;
+        	num /= 10;
+		}
+		if(a[i] == rev_num)
+		{
+			continue;
+		}
+		a[j] = a[i];
+		j++;
+	}
+	n = j;
+
+	for(int i=0;i<n;i++)
+	{
+		bool prime = true;
+		if (a[i] < 3)
+			prime = false;
+		for(int j=2;j*j<=a[i];j++)
+		{
+			if (a[i] % j == 0)
+			{
+				prime = false;
+				break;
+			}
+		}
+		if(prime)
+		{
+			for(int j=n;j>i;j--)
+			{
+				a[j] = a[j-1];
+			}
+			i++;
+			n++;
+		}
+	}
+
+
+	// // for(int i=0;i<n-1;i++)
+	// // 	for(int j=i+1;j<n;j++)
+	// // 	{
+    // //     if (a[i] < a[j])
+	// // 		{
+	// // 			int tmp = a[i];
+	// // 			a[i] = a[j];
+	// // 			a[j] = tmp;
+	// // 		}
+	// // 	}
+
+	for(int i=0;i<n;i++)
+		std::cout << a[i] << std::endl;
+
+
 	return 0;
+
 }
