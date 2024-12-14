d77741b4229a2d1c7aa145c586130938f9be34a8
DeYan009 <124430267+DeYan009@users.noreply.github.com>
2024-12-14 14:56:06 +0200

    notfixed

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -11,16 +11,19 @@ int main()
     // std::cout << mt::isSorted(head);
     if(mt::isSorted(head))
     {
-        mt::Node* current = head;
-        int i = 1;
+        mt::Node* current = head->next;
+        int i = 0;
 		while(current != nullptr)
         {
-            std::cout << current->data << " " << i  << " " << mt::ContainsDigits246(current->data) <<std::endl;
+            mt::Print(head);
+            // std::cout << current->data << " " << i  << " " << mt::ContainsDigits246(current->data) <<std::endl;
+            std::cout << "****" << std::endl;
             if(!mt::ContainsDigits246(current->data))
             {
                 std::cout << "<" << current->data << ">" <<std::endl;
                 mt::Remove(head, i);
-                current = current->next;
+                // current = current->next;
+                std::cout << ">" << current->data << "<" <<std::endl;
                 // current = head;
                 // i = 1;
                 // std::cout << current->data <<std::endl;
