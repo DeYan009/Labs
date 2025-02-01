be40adea5407456683f89eebc12b5dd7f006427b
DeYan009 <124430267+DeYan009@users.noreply.github.com>
2025-02-01 14:36:41 +0200

    add lab12 ex2 and ex3(quick sort doesnt work)

--- lab12/main.cpp
+++ lab12/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 
 int lenstring(std::string s)
 {
@@ -10,15 +10,93 @@ int lenstring(std::string s)
     return 0;
 }
 
+void QuickSort(int a, int b, int* arr)
+{
+    if(a>=b)
+        return;
+    int k = (rand()*rand()) % (b-a+1) + a;
+    int m = arr[k];
+
+    int l = a - 1;
+    int r = b + 1;
+    while(true) {
+        do l++; while(arr[l] < m);
+        do r--; while(arr[r] > m);
+        if(l >= r) 
+            break;
+        std::swap(arr[l], arr[r]);
+    }
+    l = r;
+    r++;
+    QuickSort(a, l, arr);
+    QuickSort(r, b, arr);
+}
+
+void Search(int i, int j, int step, int n, int m, char field[100][100], int table[100][100])
+{
+    if(step >= table[i][j])
+        return;
+
+    table[i][j] = step;
+
+    if(field[i][j] == 'E')
+        return;
+
+    if(i + 1 < n && field[i+1][j] != '#')
+        Search(i+1,j,step+1,n,m,field,table);
+    if(i - 1 >= 0 && field[i-1][j] != '#')
+        Search(i-1,j,step+1,n,m,field,table);
+    if(j + 1 < m && field[i][j + 1] != '#')
+        Search(i,j+1,step+1,n,m,field,table);
+    if(j - 1 >= 0 && field[i][j - 1] != '#')
+        Search(i,j-1,step+1,n,m,field,table);
+}
+
 int main() 
 {
-    // ex 1
-    std::string s;
-    std::cin >> s;
-    std::cout << lenstring(s);
-    // std::cout << lenstring("");
+    // // ex 1
+    // std::string s;
+    // std::cin >> s;
+    // std::cout << lenstring(s);
+    // // std::cout << lenstring("");
 
     // ex 2
-    std::cout << "hello world" << std::endl;
+    int s[100] = {1, 2, 4 , 8, 7, 4};
+    QuickSort(1, 2, s);
+    for(int i=0;i<6;i++)
+        std::cout << s[i];
+
+    // // ex 3
+    // char field[100][100];
+    // int table[100][100];
+
+    // std::ifstream in("input.txt");
+    // int n, m;
+    // int is, js;
+    // int ie, je;
+    // in >> n >> m;
+    // for(int i=0;i<n;i++)
+    //     for(int j=0;j<m;j++) {
+    //         table[i][j] = INT_MAX;
+    //         in >> field[i][j];
+    //         if(field[i][j] == 'S') {
+    //             is = i;
+    //             js = j;
+    //         }
+    //         else if(field[i][j] == 'E') {
+    //             ie = i;
+    //             je = j;
+    //         }
+    //     }
+
+    // Search(is, js, 0, n, m, field, table);
+    // std::cout << table[ie][je] << std::endl;
+    
+    // for(int i=0;i<n;i++)
+    // {
+    //     for(int j=0;j<m;j++)
+    //         std::cout << field[i][j] << ' ';
+    //     std::cout << std::endl;
+    // }
 	return 0;
 }
